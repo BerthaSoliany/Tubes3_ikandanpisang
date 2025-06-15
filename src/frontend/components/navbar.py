cc1dac289201111c9634fa30f27c1e1e0dd16a80
Bertha Soliany Frandi <163696570+BerthaSoliany@users.noreply.github.com>
2025-06-15 14:10:40 +0700

    fix: fix routing

--- src/frontend/components/navbar.py
+++ src/frontend/components/navbar.py
@@ -1,28 +1,15 @@
 import flet as ft
 
+class Routes:
+    HOME = "/"
+    SEARCH = "/search"
+
 def create_navbar(page: ft.Page):
-    # page.fonts = {
-    #     "PGO": "fonts/Pathway_Gothic_One/PathwayGothicOne-Regular.ttf",
-    # }
-
-    # page.theme = ft.Theme(font_family="PGO")
-
-    # def flush_session(e):
-    #     # contoh
-    #     page.session.set("Tanaman", None)
-    #     page.session.set("Sorting", None)
-    #     page.session.set("action", None)
-    #     page.session.set("jenis_tanaman", None)
-    #     page.session.set("index_tanaman", None)
-    #     page.session.set("data_pertumbuhan_tanaman", None)
-
     def on_home_click(e):
-        # flush_session(e)
-        page.go("/src/frontend/pages/home")
+        page.go(Routes.HOME)
 
     def on_search_click(e):
-        # flush_session(e)
-        page.go("/src/frontend/pages/searchPage")   
+        page.go(Routes.SEARCH)
 
     return ft.Container(
         content=ft.Row(
